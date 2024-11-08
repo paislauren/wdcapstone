f7d5309498f8f18e0a759acfba4bcfe3fd8dd2bb
paislauren <166541104+paislauren@users.noreply.github.com>
2024-11-08 21:18:42 +0000

    Initial Commit - Klondike Teacher Hub

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log("Welcome to the Teacher Resource Hub!");
+function loadAnnouncements() {
+    // Code to load announcements from a database or API can go here
+    console.log("Loading announcements...");
+}
+
+// Call the function (in the future this will actually do something!)
+loadAnnouncements();
